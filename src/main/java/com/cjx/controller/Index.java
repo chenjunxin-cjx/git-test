081cd782baed377f0a60775640a4a5e05748d222
cjx <cjx@chenjunxin.com>
2022-02-01 19:14:34 +0800

    使用idea进行git的提交操作测试-版本二，加入了hello git

--- src/main/java/com/cjx/controller/Index.java
+++ src/main/java/com/cjx/controller/Index.java
@@ -3,5 +3,7 @@ package com.cjx.controller;
 public class Index {
     public static void main(String[] args) {
         System.out.println("abcdefg");
+        System.out.println("hello git");
+
     }
 }
